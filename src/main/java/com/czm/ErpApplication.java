42794335f8ee93918fd33eaf9403a6bc381754b7
czm <zimingmorning520@gmail.com>
2024-04-28 22:28:50 -0400

    erp application commit no1

---
+++ src/main/java/com/czm/ErpApplication.java
@@ -0,0 +1,12 @@
+package com.czm;
+
+/**
+ * @author Ziming Chen
+ * @version 1.0
+ */
+public class ErpApplication {
+    public static void main(String[] args) {
+
+        System.out.println("ErpApplication hello 100 ");
+    }
+}
