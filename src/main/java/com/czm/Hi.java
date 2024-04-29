8fcd4d1528e23eef9aebef185c5be3605f8187ac
czm <zimingmorning520@gmail.com>
2024-04-28 23:10:01 -0400

    hi commit no3

---
+++ src/main/java/com/czm/Hi.java
@@ -0,0 +1,14 @@
+package com.czm;
+
+/**
+ * @author Ziming Chen
+ * @version 1.0
+ */
+public class Hi {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("this is hi.class from v3.0");
+    }
+}
